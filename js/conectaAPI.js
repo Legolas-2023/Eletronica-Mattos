d5c8c482f1080c3bec70c13648cef05683ba4fae
Crystian <crystianmattos20@gmail.com>
2024-07-11 17:58:31 -0300

    Update conectaAPI.js

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -1,11 +1,11 @@
 async function Placas(){
-    const conexaoPlacas = await fetch('https://api.npoint.io/73c9bbfe60607761b225/Placas/0');
+    const conexaoPlacas = await fetch('https://api.npoint.io/73c9bbfe60607761b225/Placas');
     const conexaoConvertidaPlacas = await conexaoPlacas.json();
     return conexaoConvertidaPlacas
 }
 
 async function adicionarPlacas(nome, tv, modelo, quantidade, caixa){
-    const conexaoPlacas = await fetch('https://api.npoint.io/73c9bbfe60607761b225/Placas/0', {
+    const conexaoPlacas = await fetch('https://api.npoint.io/73c9bbfe60607761b225/Placas', {
         method:'POST',
         headers:{
             'Content-type': 'application/json'
