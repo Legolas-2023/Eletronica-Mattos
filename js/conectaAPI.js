488ff70e727b8c087cc6f16473eb263bb82b1ef5
Crystian <crystianmattos20@gmail.com>
2024-07-11 16:18:38 -0300

    Update conectaAPI.js

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -1,11 +1,11 @@
 async function Placas(){
-    const conexaoPlacas = await fetch('https://legolas-2023.github.io/Eletronica-Mattos/db.json');
+    const conexaoPlacas = await fetch('https://legolas-2023.github.io/Eletronica-Mattos/db.json?q=Placas');
     const conexaoConvertidaPlacas = await conexaoPlacas.json();
     return conexaoConvertidaPlacas
 }
 
 async function adicionarPlacas(nome, tv, modelo, quantidade, caixa){
-    const conexaoPlacas = await fetch('https://legolas-2023.github.io/Eletronica-Mattos/db.json', {
+    const conexaoPlacas = await fetch('https://legolas-2023.github.io/Eletronica-Mattos/db.json?q=Placas', {
         method:'POST',
         headers:{
             'Content-type': 'application/json'
