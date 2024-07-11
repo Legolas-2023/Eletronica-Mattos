c93137f59be9e76fadf9a183d08382ec1ce01a45
Crystian <crystianmattos20@gmail.com>
2024-07-11 17:52:37 -0300

    Update conectaAPI.js

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -1,11 +1,11 @@
 async function Placas(){
-    const conexaoPlacas = await fetch('https://legolas-2023.github.io/DB-JSON-EM/Placas');
+    const conexaoPlacas = await fetch('https://api.npoint.io/73c9bbfe60607761b225/Placas/0');
     const conexaoConvertidaPlacas = await conexaoPlacas.json();
     return conexaoConvertidaPlacas
 }
 
 async function adicionarPlacas(nome, tv, modelo, quantidade, caixa){
-    const conexaoPlacas = await fetch('https://legolas-2023.github.io/DB-JSON-EM/Placas', {
+    const conexaoPlacas = await fetch('https://api.npoint.io/73c9bbfe60607761b225/Placas/0', {
         method:'POST',
         headers:{
             'Content-type': 'application/json'
