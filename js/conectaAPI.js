8cc15d14f251c60915a0931ebfa4a5f6594d0f15
Crystian <crystianmattos20@gmail.com>
2024-07-12 16:00:58 -0300

    Update conectaAPI.js

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -1,11 +1,11 @@
 async function Placas(){
-    const conexaoPlacas = await fetch('https:// 6691797d26c2a69f6e8fda80 .mockapi.io / Placas');
+    const conexaoPlacas = await fetch('https://6691797d26c2a69f6e8fda80.mockapi.io/Placas');
     const conexaoConvertidaPlacas = await conexaoPlacas.json();
     return conexaoConvertidaPlacas
 }
 
 async function adicionarPlacas(nome, tv, modelo, quantidade, caixa){
-    const conexaoPlacas = await fetch('https:// 6691797d26c2a69f6e8fda80 .mockapi.io / Placas', {
+    const conexaoPlacas = await fetch('https://6691797d26c2a69f6e8fda80.mockapi.io/Placas', {
         method:'POST',
         headers:{
             'Content-type': 'application/json'
